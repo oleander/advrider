afd0baa934e6af63711df288ec20f1fe95db7333
Linus Oleander <linus@oleander.io>
2024-05-05 10:49:04 +0200

    Remove unnecessary dependencies, update `spider` features for more focused usage, modify scraper's URL and OpenAI model version, and disable web caching for scraper functionality. Update `header` function to set request headers appropriately for the scraper.

--- src/bin/scraper.rs
+++ src/bin/scraper.rs
@@ -4,11 +4,11 @@ use anyhow::{Context, Result};
 use spider::website::Website;
 use spider::tokio;
 
-const URL: &str = "https://advrider.com/f/threads/husqvarna-701-super-moto-and-enduro.1086621";
-const OPENAI_MODEL: &str = "gpt-4-turbo-preview";
+const URL: &str = "https://advrider.com/f/threads/husqvarna-701-super-moto-and-enduro.1086621/page-[1-2314]";
+const OPENAI_MODEL: &str = "gpt-3.5-turbo";
 const CRAWL_LIST: [&str; CAPACITY] = [URL];
 const AGENT_NAME: &str = "Lisa Eriksson";
-const ENABLE_WEB_CACHE: bool = true;
+const ENABLE_WEB_CACHE: bool = false;
 const OPENAI_MAX_TOKEN: u16 = 512;
 const RESPECT_ROBOT: bool = true;
 const REDIRECT_LIMIT: usize = 2;
@@ -44,9 +44,19 @@ async fn main() -> Result<()> {
 async fn fetch(url: &str) -> Result<String> {
   log::info!("Starting ...");
 
-  let mut webpage = webpage(url).await?;
-  webpage.crawl().await;
-  let body = webpage.get_pages().context("No web page received")?;
+  let mut website = Website::new(url)
+    .with_openai(openai().await.ok())
+    // .with_caching(true)
+    .with_subdomains(false)
+    .with_redirect_limit(2)
+    .with_headers(header()?)
+    .with_config(config())
+    .build()
+    .context("Could not build webpage")?;
+
+  website.scrape().await;
+
+  let body = website.get_pages().context("No web page received")?;
 
   let mut content = String::new();
   for page in (*body).iter() {
@@ -59,16 +69,6 @@ async fn fetch(url: &str) -> Result<String> {
   log::info!("Done downloading {} bytes", len);
 
   Ok(content)
-}
-
-fn header() -> Result<Option<HeaderMap>> {
-  let mut headers = HeaderMap::new();
-  headers.insert(header::ACCEPT, "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7".parse().unwrap());
-  headers.insert(header::ACCEPT_LANGUAGE, "en-GB,en-US;q=0.9,en;q=0.8,sv;q=0.7".parse().unwrap());
-  headers.insert(header::COOKIE, "_gcl_au=1.1.82089729.1713720688; _gid=GA1.2.1985556420.1713720688; xf_logged_in=1; xf_session=867c856b44b55341ea9c2e9b34fe6808; _ga_BCFR910NDY=GS1.1.1713720688.1.1.1713722217.0.0.0; _ga=GA1.2.469143051.1713720688".parse().unwrap());
-  headers.insert(header::UPGRADE_INSECURE_REQUESTS, "1".parse().unwrap());
-  headers.insert(header::USER_AGENT, "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36 OPR/109.0.0.0".parse()?);
-  Ok(Some(headers))
 }
 
 async fn openai() -> Result<GPTConfigs> {
@@ -86,12 +86,12 @@ fn config() -> Configuration {
     .build()
 }
 
-async fn webpage(url: &str) -> Result<Website> {
-  Website::new(url)
-    .with_openai(openai().await.ok())
-    .with_headers(header()?)
-    .with_config(config())
-    .with_caching(ENABLE_WEB_CACHE)
-    .build()
-    .context("Could not build webpage")
+fn header() -> Result<Option<HeaderMap>> {
+  let mut headers = HeaderMap::new();
+  headers.insert(header::ACCEPT, "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7".parse().unwrap());
+  headers.insert(header::ACCEPT_LANGUAGE, "en-GB,en-US;q=0.9,en;q=0.8,sv;q=0.7".parse().unwrap());
+  headers.insert(header::COOKIE, "_gcl_au=1.1.82089729.1713720688; _gid=GA1.2.1985556420.1713720688; xf_logged_in=1; xf_session=867c856b44b55341ea9c2e9b34fe6808; _ga_BCFR910NDY=GS1.1.1713720688.1.1.1713722217.0.0.0; _ga=GA1.2.469143051.1713720688".parse().unwrap());
+  headers.insert(header::UPGRADE_INSECURE_REQUESTS, "1".parse().unwrap());
+  headers.insert(header::USER_AGENT, "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36 OPR/109.0.0.0".parse()?);
+  Ok(Some(headers))
 }
