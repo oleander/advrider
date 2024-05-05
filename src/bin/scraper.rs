1471192bb310b49e8a6a9e1ec0180f09a8615e0a
Linus Oleander <linus@oleander.io>
2024-05-05 09:14:19 +0200

    Introduce spider dependency, implement basic scraper functionality, update summerize with increased input size, and adjust threading configuration

---
+++ src/bin/scraper.rs
@@ -0,0 +1,84 @@
+use std::time::Instant;
+use std::io::Error;
+
+use reqwest::header::{self, HeaderMap, HeaderValue};
+use serde_json::json;
+use spider::configuration::{Configuration, GPTConfigs};
+use spider::website::Website;
+use spider::tokio;
+use anyhow::{Context, Result};
+
+const CAPACITY: usize = 1;
+const CRAWL_LIST: [&str; CAPACITY] = ["https://advrider.com/f/threads/husqvarna-701-super-moto-and-enduro.1086621"];
+
+fn header() -> Result<HeaderMap> {
+  let mut headers = HeaderMap::new();
+  headers.insert(header::ACCEPT, "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7".parse().unwrap());
+  headers.insert(header::ACCEPT_LANGUAGE, "en-GB,en-US;q=0.9,en;q=0.8,sv;q=0.7".parse().unwrap());
+  headers.insert(header::COOKIE, "_gcl_au=1.1.82089729.1713720688; _gid=GA1.2.1985556420.1713720688; xf_logged_in=1; xf_session=867c856b44b55341ea9c2e9b34fe6808; _ga_BCFR910NDY=GS1.1.1713720688.1.1.1713722217.0.0.0; _ga=GA1.2.469143051.1713720688".parse().unwrap());
+  headers.insert(header::UPGRADE_INSECURE_REQUESTS, "1".parse().unwrap());
+  headers.insert(header::USER_AGENT, "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36 OPR/109.0.0.0".parse()?);
+  Ok(headers)
+}
+
+
+#[tokio::main]
+async fn main() -> Result<()> {
+
+  let config = Configuration::new()
+    .with_user_agent(Some("SpiderBot"))
+    .with_blacklist_url(Some(Vec::from(["https://spider.cloud/resume".into()])))
+    .with_subdomains(false)
+    .with_tld(false)
+    .with_redirect_limit(3)
+    .with_respect_robots_txt(true)
+    .with_external_domains(Some(Vec::from(["http://loto.rsseau.fr/"].map(|d| d.to_string())).into_iter()))
+    .build();
+
+  let mut handles = Vec::with_capacity(CAPACITY);
+
+  let system = std::fs::read_to_string("/Users/linus/.config/fabric/patterns/summarize/system.md").unwrap();
+  let openai = Some(GPTConfigs::new("gpt-4-turbo-preview", &system, 512));
+  for website_url in CRAWL_LIST {
+    let req = Website::new(website_url)
+      .with_openai(openai.clone())
+      .with_config(config.to_owned())
+      .with_headers(Some(header()?.clone()))
+      .with_caching(true)
+      .build();
+
+    match req {
+      Ok(mut website) => {
+        let handle = tokio::spawn(async move {
+          println!("Starting Crawl - {:?}", website.get_url().inner());
+
+          let start = Instant::now();
+          website.crawl().await;
+          let duration = start.elapsed();
+
+          let links = website.get_links();
+
+          for link in links {
+            println!("- {:?}", link.as_ref());
+          }
+
+          println!(
+            "{:?} - Time elapsed in website.crawl() is: {:?} for total pages: {:?}",
+            website.get_url().inner(),
+            duration,
+            links.len()
+          );
+        });
+
+        handles.push(handle);
+      }
+      Err(e) => println!("{:?}", e)
+    }
+  }
+
+  for handle in handles {
+    let _ = handle.await;
+  }
+
+  Ok(())
+}
--- src/bin/summerize.rs
+++ src/bin/summerize.rs
@@ -24,7 +24,7 @@ const ARTICLE: &str = include_str!("../../examples/article.md");
 const MAP_PROMPT: &str = include_str!("../../prompts/map.md");
 // const MAX_CONTEXT_SIZE: usize = 3048;
 const MODEL_NAME: &str = "gpt-3.5-turbo";
-const MAX_INPUT_SIZE: usize = 1000;
+const MAX_INPUT_SIZE: usize = 2000000_usize;
 // const TEMP: f32 = 0.1;
 
 type ID = i64;
@@ -78,7 +78,7 @@ async fn main() -> Result<()> {
   let map_prompt = Step::for_prompt_template(prompt!(MAP_PROMPT, "\n{{text}}"));
   let reduce_prompt = Step::for_prompt_template(prompt!(REDUCE_PROMPT, "\n{{text}}"));
   let chain = Chain::new(map_prompt, reduce_prompt);
-  let body = posts.body().lines().take(100).collect::<Vec<&str>>().join("\n");
+  let body = posts.body().chars().take(MAX_INPUT_SIZE).collect::<String>();
 
   let options = options!(
     ThrottleDelay: Duration::from_millis(400),
@@ -86,7 +86,7 @@ async fn main() -> Result<()> {
     MaxContextSize: 3048_usize,
     MaxBatchSize: 3000_usize,
     Temperature: 0.01_f32,
-    NThreads: 1_usize,
+    NThreads: 3_usize,
     Model: model
   );
 
