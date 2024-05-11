4429a3f6d3c26bafcacb37650d5ba109eefc7c5c
Linus Oleander <linus@oleander.io>
2024-05-11 08:57:28 +0200

    Add cache feature and implement caching in spider project

--- src/bin/glob.rs
+++ src/bin/glob.rs
@@ -1,49 +1,54 @@
 //! `cargo run --example url_glob --features glob`
 extern crate spider;
 
-use spider::configuration::Configuration;
-use spider::tokio;
-use spider::website::Website;
 use std::time::Instant;
 use std::vec;
 
+use spider::configuration::Configuration;
+use spider::tokio;
+use spider::website::Website;
+
+use crate::spider::http_cache_reqwest::CacheManager;
+
 #[tokio::main]
 async fn main() {
-    let mut b = Configuration::new();
-    let config = b.with_depth(1).with_proxies(vec!["socks5://127.0.0.1:9050".to_string()].into());
-    let mut website: Website = Website::new(
-      "https://advrider.com/f/threads/thinwater-escapades.1502022/page-[1-5]"
-    );
+  let mut b = Configuration::new();
+  let config = b
+    .with_depth(1)
+    .with_proxies(vec!["socks5://127.0.0.1:9050".to_string()].into())
+    .with_caching(true);
 
-    website
-      .configuration
-      .blacklist_url
-      .insert(Default::default())
-      .push("https://github.com/oleander".into());
+  let mut website: Website = Website::new("https://advrider.com/f/threads/thinwater-escapades.1502022/page-[1-5]");
 
-    let mut website = website.with_config(config.clone());
+  website
+    .configuration
+    .blacklist_url
+    .insert(Default::default())
+    .push("https://github.com/oleander".into());
 
-    let start = Instant::now();
-    website.scrape().await;
-    let duration = start.elapsed();
+  let website = website.with_config(config.clone()).with_caching(true);
 
-    let links = website.get_links();
-    let Some(pages) = website.get_pages() else {
-        println!("No pages found");
-        return;
-    };
+  let start = Instant::now();
+  website.scrape().await;
+  let duration = start.elapsed();
 
-    for page in pages.iter() {
-        println!("Page");
+  let links = website.get_links();
+  let Some(pages) = website.get_pages() else {
+    println!("No pages found");
+    return;
+  };
+
+  for page in pages.iter() {
+    if let Some(bytes) =  page.get_bytes() {
+      println!("Page size is: {:?}", bytes.len());
+    } else {
+      println!("Nothing returned");
     }
+  }
 
-    for link in links {
-        println!("- {:?}", link.as_ref());
-    }
+  for link in links {
+    println!("- {:?}", link.as_ref());
+  }
 
-    println!(
-        "Time elapsed in website.crawl() is: {:?} for total pages: {:?}",
-        duration,
-        links.len()
-    )
+  println!("Time elapsed in website.crawl() is: {:?} for total pages: {:?}", duration, links.len())
 }
