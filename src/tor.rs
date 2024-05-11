ba56931c7635c407496606fae100bc607469035e
Linus Oleander <linus@oleander.io>
2024-05-11 12:28:25 +0200

    Refactor Command structure and streamline authentication process

--- src/tor.rs
+++ src/tor.rs
@@ -20,9 +20,8 @@ pub struct Control {
 
 impl Control {
   pub async fn new(address: &str) -> Result<Self> {
-    let stream = TcpStream::connect(address).await?;
     Ok(Self {
-      stream
+      stream: TcpStream::connect(address).await?
     })
   }
 
@@ -30,8 +29,8 @@ impl Control {
     self.stream.write_all(command.as_bytes()).await?;
     self.stream.write_all(b"\n").await?;
     self.stream.flush().await?;
-    let actual = self.response().await?;
 
+    let actual = self.response().await?;
     if actual == expected {
       return Ok(());
     }
@@ -43,44 +42,23 @@ impl Control {
     let mut reader = BufReader::new(&mut self.stream);
     let mut response = String::new();
     reader.read_line(&mut response).await?;
-    log::debug!("Received response: '{}'", response);
     Ok(response.trim().to_string())
   }
 }
 
 pub struct Command {
-  control:  Control,
-  password: String,
-  open:     bool
+  control: Control,
+  auth:    String,
+  open:    bool
 }
 
-impl Command {
-  pub async fn new(address: &str, password: &str) -> Result<Self> {
-    Ok(Self {
-      control: Control::new(address).await?,
-      password: password.to_string(),
-      open: false
-    })
-  }
+pub struct Authenticated {
+  control: Control
+}
 
-  pub async fn authenticate(mut self) -> Result<Self> {
-    if self.open {
-      return Ok(self);
-    }
-
-    let auth = format!("AUTHENTICATE \"{}\"", self.password);
-    self.send(&auth, "250 OK").await?;
-    self.open = true;
-    Ok(self)
-  }
-
+impl Authenticated {
   pub async fn quit(mut self) -> Result<()> {
-    if !self.open {
-      return Ok(());
-    }
-
     self.send("QUIT", "250 closing connection").await?;
-    self.open = false;
     Ok(())
   }
 
@@ -109,6 +87,29 @@ impl Command {
   pub async fn refresh(&mut self) -> Result<()> {
     self.newnym().await?;
     self.wait_for_ready().await
+  }
+
+  pub async fn send(&mut self, command: &str, expected: &str) -> Result<()> {
+    log::debug!("Sending command '{}' and expecting '{}'", command, expected);
+    self.control.send(command, expected).await
+  }
+}
+
+impl Command {
+  pub async fn new(address: &str, password: &str) -> Result<Self> {
+    Ok(Self {
+      control: Control::new(address).await?,
+      auth:    format!("AUTHENTICATE \"{}\"", password),
+      open:    false
+    })
+  }
+
+  pub async fn authenticate(mut self) -> Result<Authenticated> {
+    self.send(&self.auth.clone(), "250 OK").await?;
+
+    Ok(Authenticated {
+      control: self.control
+    })
   }
 
   pub async fn send(&mut self, command: &str, expected: &str) -> Result<()> {
