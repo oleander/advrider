4ebb2af432559b125794224f5289c47e5c148951
Linus Oleander <linus@oleander.io>
2024-05-11 13:13:23 +0200

    Add colored 2.1.0 dependency and improve Tor control connection logic

--- src/tor.rs
+++ src/tor.rs
@@ -1,10 +1,11 @@
 use std::time::Duration;
 
 use async_trait::async_trait;
+use colored::*;
 use tokio::io::{self, AsyncBufReadExt, AsyncWriteExt, BufReader};
 use tokio::net::TcpStream;
 use thiserror::Error;
-use anyhow::Result;
+use anyhow::{bail, Result};
 use tokio::time::sleep;
 
 #[derive(Error, Debug)]
@@ -15,27 +16,52 @@ pub enum ControlError {
   CommandError(String, String, String)
 }
 
+const RETRIES: usize = 10;
+const PAUSE: Duration = Duration::from_secs(1);
+
 pub struct Control {
   stream: TcpStream
 }
 
 impl Control {
   pub async fn new(address: &str) -> Result<Self> {
-    Ok(Self {
-      stream: TcpStream::connect(address).await?
-    })
+    Self::new_and_retry(address, 0).await
+  }
+
+  pub async fn new_and_retry(address: &str, retries: usize) -> Result<Self> {
+    if retries >= RETRIES {
+      bail!("Failed to connect to Tor control at {} (retries: {})", address, retries);
+    } else if retries > 0 {
+      log::warn!("Retrying connection to Tor control at {} @ {}", address, retries);
+      sleep(PAUSE).await;
+    } else {
+      log::info!("Connecting to Tor control at {} @ {}", address, retries);
+    }
+
+    match TcpStream::connect(address).await {
+      Ok(stream) => {
+        return Ok(Self {
+          stream
+        });
+      }
+      _ => return Box::pin(Self::new_and_retry(address, retries + 1)).await
+    }
   }
 
   pub async fn send(&mut self, command: &str, expected: &str) -> Result<()> {
-    log::debug!("Sending command '{}' and expecting '{}'", command, expected);
+    log::debug!("[{}] -> ({})", command.blue(), expected.green());
 
     self.stream.write_all(command.as_bytes()).await?;
     self.stream.write_all(b"\n").await?;
     self.stream.flush().await?;
 
     let actual = self.response().await?;
+
     if actual == expected {
+      log::debug!("[{}] -> ({}) {}", command.blue(), expected.green(), actual.yellow());
       return Ok(());
+    } else {
+      log::error!("[{}] -> ({}) {}", command.blue(), expected.red(), actual.yellow());
     }
 
     Err(ControlError::CommandError(expected.to_string(), command.to_string(), actual).into())
